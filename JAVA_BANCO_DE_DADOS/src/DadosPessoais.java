ce1d13f482e6f353804f9746e869aa5b7a0b18c5
Arthur Queiroz <143663976+ArthurLimaQz@users.noreply.github.com>
2025-03-03 18:23:21 -0300

    1 att.

---
+++ JAVA_BANCO_DE_DADOS/src/DadosPessoais.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class DadosPessoais {
+    String Nome;
+    String Nome_Genitor;
+    String Nome_Genitora;
+    Date Data_Nascimento;
+    
+}
---
+++ JAVA_BANCO_DE_DADOS/src/LivroRegistro.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class LivroRegistro {
+    String Livro;
+    int Folha;
+    Date Data;
+    int Termo;   
+}
